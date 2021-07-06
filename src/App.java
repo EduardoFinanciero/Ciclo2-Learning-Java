488c3d846baf86298040341840b40298381bee80
EduardoFinanciero <eduardo.mifinanciero@gmail.com>
2021-07-05 23:05:52 -0500

    Update Monday from Teacher

--- src/App.java
+++ src/App.java
@@ -1,5 +1,34 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+// import java.lang.invoke.SwitchPoint;
+// import java.net.SocketPermission;
+
+// public class App {
+
+//     private String nombre;
+//     private int edad;
+//     private char sexo;
+//     private double estatura;
+//     private float peso;
+//     private boolean estadoCivil;
+    
+
+
+    // public static void main(String[] args) throws Exception { 
+    //     System.out.println("Hello, World!");
+    //     System.out.println();
+    // int resultado = 15+32;
+    // String res = "Hola";
+    // int resultado = 15 +32;
+    // int resultado3 = 125 -32;
+    // double 
+
+
+
+
+
+
+
+    
+    // }
+
+
+
